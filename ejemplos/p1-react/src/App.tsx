b2c896138de862b53fb7984da35f268cd430d930
GastonQuelali <gaston_q@hotmail.com>
2026-02-05 12:43:51 -0400

    Estilos

--- ejemplos/p1-react/src/App.tsx
+++ ejemplos/p1-react/src/App.tsx
@@ -1,7 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 
+const inputs: React.CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  margin: "2px 2px 20px 2px",
+  gap: "5px"
+};
+
+const buttons: React.CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  gap: "15px",
+  margin: "30px 10px"
+};
+
 function App() {
+
   const [numero1, setNumero1] = useState<number>()
   const [numero2, setNumero2] = useState<number>()
   const [resultado, setResultado] = useState<number | string>("N/D")
@@ -37,26 +54,27 @@ function App() {
     <>
       <h1>Version Calculadora 1.0</h1>
 
-      <div>
-        <label>Numero: </label>
+      <div style={inputs}>
         <input
-          type="text" 
-          value={numero1} 
+          type="text"
+          value={numero1}
           onChange={(e) => setNumero1(Number(e.target.value))}
+          placeholder='Numero 1'
         />
-        <label>Numero: </label>
+
         <input
-          type='text' 
-          value={numero2} 
+          type='text'
+          value={numero2}
           onChange={(e) => setNumero2(Number(e.target.value))}
+          placeholder='Numero 2'
         />
       </div>
 
-      <div>
-         <button onClick={() => calcular('+')}>+</button>
-         <button onClick={() => calcular('-')}>-</button>
-         <button onClick={() => calcular('*')}>*</button>
-         <button onClick={() => calcular('/')}>/</button>
+      <div style={buttons}>
+        <button onClick={() => calcular('+')}>+</button>
+        <button onClick={() => calcular('-')}>-</button>
+        <button onClick={() => calcular('*')}>*</button>
+        <button onClick={() => calcular('/')}>/</button>
       </div>
 
       <hr />
